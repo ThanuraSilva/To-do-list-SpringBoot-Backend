ad2ad5d82c00cbcc107d10db7e009eda11ccbe89
ThanuraSilva <thanurasilva1989@gmail.com>
2022-06-27 05:54:43 +0530

    Update the entity of ToDO with new two fields expose

--- src/main/java/com/thanura/dep8/entity/ToDO.java
+++ src/main/java/com/thanura/dep8/entity/ToDO.java
@@ -6,6 +6,8 @@ import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Time;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,6 +19,8 @@ public class ToDO {
     private int id;
     @Column(nullable = false)
     private String todo;
+    private Date date;
+    private Time time;
 
     @ManyToOne
     @JoinColumn(name = "user_id",referencedColumnName = "id",nullable = false)
@@ -25,5 +29,11 @@ public class ToDO {
     public ToDO(int id, String todo) {
         this.id = id;
         this.todo = todo;
+    }
+
+    public ToDO(int id, String todo, User user) {
+        this.id = id;
+        this.todo = todo;
+        this.user = user;
     }
 }
