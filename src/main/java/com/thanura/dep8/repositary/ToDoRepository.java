936437f469c0076ea4706e31d2d34e6d768d9b22
ThanuraSilva <thanurasilva1989@gmail.com>
2022-06-27 05:46:24 +0530

    Finalize repository layer

--- src/main/java/com/thanura/dep8/repositary/ToDoRepository.java
+++ src/main/java/com/thanura/dep8/repositary/ToDoRepository.java
@@ -1,5 +1,13 @@
 package com.thanura.dep8.repositary;
 
+import com.thanura.dep8.entity.ToDO;
+import com.thanura.dep8.entity.User;
+
+import java.util.List;
+
 public interface ToDoRepository {
+    List<ToDO>findAllToDosByUser(User user);
+
+    long countToDosByUser(User user);
 
 }
