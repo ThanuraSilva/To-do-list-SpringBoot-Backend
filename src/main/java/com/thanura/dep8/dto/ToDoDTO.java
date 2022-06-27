87505066aaa2f537fcd937d3df9a630e35666889
ThanuraSilva <thanurasilva1989@gmail.com>
2022-06-27 05:34:30 +0530

    UserDTO and ToDoDTO DTO Created

--- src/main/java/com/thanura/dep8/dto/ToDoDTO.java
+++ src/main/java/com/thanura/dep8/dto/ToDoDTO.java
@@ -1,4 +1,15 @@
 package com.thanura.dep8.dto;
 
+import com.thanura.dep8.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ToDoDTO {
+    private int id;
+    private String todo;
+    private String userId;
 }
--- src/main/java/com/thanura/dep8/dto/UserDTO.java
+++ src/main/java/com/thanura/dep8/dto/UserDTO.java
@@ -1,4 +1,15 @@
 package com.thanura.dep8.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserDTO {
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
 }
