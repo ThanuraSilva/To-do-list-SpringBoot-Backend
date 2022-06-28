422c6b9e95591a0094e1bb19e7c28683d3b0cde3
ThanuraSilva <thanurasilva1989@gmail.com>
2022-06-28 15:13:20 +0530

    Finalize ToDoController

--- src/main/java/com/thanura/dep8/api/ToDoController.java
+++ src/main/java/com/thanura/dep8/api/ToDoController.java
@@ -1,4 +1,42 @@
 package com.thanura.dep8.api;
 
+import com.thanura.dep8.dto.ToDoDTO;
+import com.thanura.dep8.service.ToDoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/users/{userId:[A-Fa-f0-9\\-]{36}}/notes")
 public class ToDoController {
+
+    public ToDoService toDoService;
+
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "application/json",produces = "application/json")
+    public ToDoDTO addToDo(@PathVariable String userId, ToDoDTO toDo, Errors errors){
+        if (errors.hasFieldErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,errors.getFieldErrors().get(0).getDefaultMessage());
+        }
+        if(!userId.equals(toDo.getUserId()))
+            throw new ResponseStatusException(HttpStatus.CONFLICT,"User Id is mismatch");
+        return toDoService.saveToDo(toDo);
+
+    }
+    @DeleteMapping(path = "{userId:\\d+}")
+    public void deleteToDo(@PathVariable String userId,int toDoId){
+        toDoService.deleteToDo(userId, toDoId);
+
+    }
+    @GetMapping(produces = "application/json")
+    public List<ToDoDTO>grtAllNotes(String userId){
+        return toDoService.getAllToDos(userId);
+    }
 }
