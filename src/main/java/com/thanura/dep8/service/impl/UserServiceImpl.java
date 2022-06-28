558b725a816a420919435d8d20e00d33efdebc63
ThanuraSilva <thanurasilva1989@gmail.com>
2022-06-28 13:42:55 +0530

    Finalize UserServiceImpl

--- src/main/java/com/thanura/dep8/service/impl/UserServiceImpl.java
+++ src/main/java/com/thanura/dep8/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.thanura.dep8.service.impl;
 
 import com.thanura.dep8.dto.UserDTO;
+import com.thanura.dep8.entity.User;
 import com.thanura.dep8.repositary.UserRepository;
 import com.thanura.dep8.service.UserService;
 import com.thanura.dep8.service.exception.DuplicateEmailException;
@@ -8,7 +9,7 @@ import com.thanura.dep8.service.exception.NotFoundException;
 import com.thanura.dep8.service.util.EntityDTOConversion;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,7 +27,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO registerUser(UserDTO userDTO) throws DuplicateEmailException {
-        //find out whether it is there existed user going to be saved
+        //find out whether it is existed user that going to be saved
         if(userRepository.existsUserByEmail(userDTO.getEmail())) throw new DuplicateEmailException("User already exists");
         //if not
         userDTO.setId(UUID.randomUUID().toString());
@@ -37,17 +38,23 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDTO updateUser(UserDTO userDTO) throws NotFoundException {
-        return null;
+    public void updateUser(UserDTO userDTO) throws NotFoundException {
+     Optional<User> optUser = userRepository.findUserById(userDTO.getId());
+     if(!optUser.isPresent()) throw new NotFoundException("No user found");
+     optUser.get().setFullName(userDTO.getFullName());
+     optUser.get().setPassword(userDTO.getPassword());
+
     }
 
     @Override
     public UserDTO getUserInfo(String userId) throws NotFoundException {
-        return null;
+       return userRepository.findUserById(userId).map(transform::getUserDTO)
+                .orElseThrow(()->new NotFoundException("Invalid User"));
     }
 
     @Override
     public void deleteUser(String userId) throws NotFoundException {
+         if(!userRepository.existUserById(userId))throw new NotFoundException("Invalid UserId");
 
     }
 }
